4947c44dd2fd6bbb3217b2e65e70bf5e2421b614
Ali-Chouchene <choucheneali9801@gmail.com>
2023-01-18 13:16:07 +0100

    boilerplate

---
+++ script/script.js
@@ -0,0 +1,35 @@
+/** 
+ Descrizione:
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+Super Bonus
+Riusciamo a gestire e mostrare un loader mentre le mail non sono ancora tutte pronte? :sorriso:
+
+**/
+
+
+
+// console.log("VUE OK", Vue);
+
+//! VUE_JS!//
+
+const app = Vue.createApp({
+    name: "Vue-Carousel",
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+
+});
+
+app.mount("#root");
